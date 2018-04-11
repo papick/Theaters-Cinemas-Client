e7f959aa2d26d03e825c7d4cbc652e2851288088
50pace <50paceg@gmail.com>
2018-04-11 20:17:40 +0200

    nis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { routes } from './app.router';
+import {DataListModule} from 'primeng/datalist';
+
+import {AccordionModule} from 'primeng/accordion';
+import {MenuItem} from 'primeng/api';
 
 import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './header-component/header-component.component';
@@ -14,6 +18,14 @@ import {AccountService} from './service/account.service';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {RepertoryComponent} from './repertory/repertory.component';
+import { FanZoneComponentComponent } from './fan-zone-component/fan-zone-component.component';
+import { NewOfferComponentComponent } from './new-offer-component/new-offer-component.component';
+import { UsedOfferComponentComponent } from './used-offer-component/used-offer-component.component';
+import {NewOfferService} from './new-offer-component/newOfferService';
+import { NewOfferWrapperComponentComponent } from './new-offer-wrapper-component/new-offer-wrapper-component.component';
+import { UsedOfferWrapperComponentComponent } from './used-offer-wrapper-component/used-offer-wrapper-component.component';
+import {UsedOfferService} from './used-offer-component/usedOfferService';
+
 
 
 
@@ -29,14 +41,20 @@ import {RepertoryComponent} from './repertory/repertory.component';
     CinemaTheaterComponentComponent,
     HomeComponent,
     RepertoryComponent,
+    FanZoneComponentComponent,
+    NewOfferComponentComponent,
+    UsedOfferComponentComponent,
+    NewOfferWrapperComponentComponent,
+    UsedOfferWrapperComponentComponent
   ],
   imports: [
     BrowserModule,
     routes,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    DataListModule
   ],
-  providers: [AccountService],
+  providers: [AccountService , NewOfferService, UsedOfferService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -6,6 +6,11 @@ import {LogInFormComponentComponent} from './log-in-form-component/log-in-form-c
 import {RegisterFormComponentComponent} from './register-form-component/register-form-component.component';
 import {CinemaTheaterComponentComponent} from './cinema-theater-component/cinema-theater-component.component';
 import {RepertoryComponent} from './repertory/repertory.component';
+import {FanZoneComponentComponent} from './fan-zone-component/fan-zone-component.component';
+import {UsedOfferComponentComponent} from './used-offer-component/used-offer-component.component';
+import {NewOfferComponentComponent} from './new-offer-component/new-offer-component.component';
+import {NewOfferWrapperComponentComponent} from './new-offer-wrapper-component/new-offer-wrapper-component.component';
+import {UsedOfferWrapperComponentComponent} from './used-offer-wrapper-component/used-offer-wrapper-component.component';
 
 export const router: Routes = [
 
@@ -13,10 +18,17 @@ export const router: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'log-in-form-component', component: LogInFormComponentComponent},
   {path: 'register-form-component', component: RegisterFormComponentComponent},
-  {path: 'cinema-theater-component', component: CinemaTheaterComponentComponent}
+  {path: 'cinema-theater-component', component: CinemaTheaterComponentComponent},
   // {path: 'repertory', component: RepertoryComponent}
+  {path: 'fan-zone-component', component: FanZoneComponentComponent ,
+    children: [
+      {path: 'used-offer-wrapper', component: UsedOfferWrapperComponentComponent},
+      {path: 'new-offer-wrapper', component: NewOfferWrapperComponentComponent}
+      ]}
+
 
 
 ];
+
 
 export  const routes: ModuleWithProviders = RouterModule.forRoot(router);
